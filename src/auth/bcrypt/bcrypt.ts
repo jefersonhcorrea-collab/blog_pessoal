94de75e53fb18f8e126021a9c03e655c5eb4524e
Jeferson correa <jefersonhcorrea@hotmail.com>
2025-12-11 08:36:34 -0300

    feat:desenvolvendo bcrypt

---
+++ src/auth/bcrypt/bcrypt.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { compare, hash } from "bcrypt";
+
+@Injectable()
+export class Bcrypt{
+    async criptografarSenha(senha: string): Promise<string>{
+    const saltos: number = 10;
+    return await hash(senha,saltos); //colocar o hash do bcrypt (CUIDADO)
+
+    }
+
+    async comparararSenha(senhaDigitada: string,senhaBanco: string): Promise<boolean>{
+            return await compare(senhaDigitada,senhaBanco);
+    }
+
+    
+
+}
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty } from "class-validator"
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn, } from "typeorm"
 import { Tema } from "../../tema/entities/tema.entity"
+import { Usuario } from "../../usuario/entities/usuario.entity"
 
 @Entity({name: "tb_postagens"})
 export class Postagem {
@@ -22,6 +23,11 @@ export class Postagem {
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE",
     })
-    tema: Tema                                                              
+    tema: Tema
+    
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem,{
+        onDelete: "CASCADE"
+    })
+    usuario: Usuario
     
 }
